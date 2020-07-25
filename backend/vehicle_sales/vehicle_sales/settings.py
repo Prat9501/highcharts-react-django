ceaf3e1abbf3d0145093e943a9aa944479729c29
Prateek Gupta <prateekg.9501@gmail.com>
2020-07-25 23:00:50 +0530

    updated readme

--- backend/vehicle_sales/vehicle_sales/settings.py
+++ backend/vehicle_sales/vehicle_sales/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v2=-1g8qlu+=jyvv#@0dltit*(^!c(_^n!&ev--73inb9)d2pt'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
